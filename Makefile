ad577219e11552645512be260c8b80f688bc8d85
Patrick Forget <patforg@oboxmedia.com>
2016-03-28 07:47:42 -0400

    added file to convert hcp files to concorde matrices

--- Makefile
+++ Makefile
@@ -8,5 +8,8 @@ hc_check: hc_check.c src/hclib.c
 hc_to_dot: hc_to_dot.c src/hclib.c
 		$(CC) -o $@ $^ $(CFLAGS)
 
+hc_to_concorde: hc_to_concorde.c src/hclib.c
+		$(CC) -o $@ $^ $(CFLAGS)
+
 hc_solve: hc_solve.c src/hclib.c src/solvers/*.c
 		$(CC) -o $@ $^ $(CFLAGS)
---
+++ hc_to_concorde.c
@@ -0,0 +1,74 @@
+/* 
+ * creates a concord matrix style for concorde 
+ * input is an edge list file output a weighted 
+ * adjacency matrix 0 if edge exists and 1 
+ * if no edge exists
+ */
+
+#include <stdio.h>
+
+#include "hclib.h"
+
+static void print_usage();
+
+int main(int argc, char *argv[])
+{
+
+    char *graph_file;
+    char *out_file;
+    FILE * fp;
+    if (argc > 1)
+    {
+        graph_file = argv[1];
+        out_file = argv[2];
+    } else
+    {
+        print_usage();
+        return 1;
+    }
+
+
+    int **graph;
+    int node_count;
+    int edge_count;
+    graph = read_graph(graph_file, &node_count, &edge_count);
+
+    printf("Graph has %d nodes and %d edges\n", node_count, edge_count);
+
+    int i;
+    int j;
+
+    fp = fopen(out_file,"w+");
+    if (!fp) {
+        printf("Could not open file for writting %s\n", out_file);
+        return 1;
+
+    }
+
+    for (i = 1; i <= node_count; i++) {
+        for (j = 1; j <= node_count; j++) {
+                fprintf(fp,"%i", (graph[i][j] ? 0 : 1) );
+            if (i > 1) {
+                fprintf(fp," ");
+            } //if
+        }
+        fputs("\n",fp);
+    }
+
+    if (fp) {
+        fclose(fp);
+    }
+
+    free_graph(graph, &node_count);
+
+    return 0;
+}
+
+static void print_usage()
+{
+    printf("Converts graphfile into concorde compatible matrix");
+
+    printf("Usage:\n");
+    printf("hc_to_dot graphfile.hcp dotfile.opt\n");
+    
+}
